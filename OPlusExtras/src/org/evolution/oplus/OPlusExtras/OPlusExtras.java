56d898ffefeca0b43b18439679c3deaa13ae91dd
Vachounet <vachounet@live.fr>
2022-10-24 14:05:39 +0000

    Allow opting in for having a menu in dolby * TODO: Make activity configurable

--- OPlusExtras/src/org/evolution/oplus/OPlusExtras/OPlusExtras.java
+++ OPlusExtras/src/org/evolution/oplus/OPlusExtras/OPlusExtras.java
@@ -73,6 +73,8 @@ public class OPlusExtras extends PreferenceFragment
     private static TwoStatePreference mAutoHBMSwitch;
     private Preference mHBMInfo;
 
+    private static final String KEY_CATEGORY_DOLBY = "dolby";
+
     public static final String KEY_CATEGORY_CPU = "cpu";
     public static final String KEY_POWER_EFFICIENT_WQ_SWITCH = "power_efficient_workqueue";
     public static final String KEY_TOUCHBOOST_SWITCH = "touchboost";
@@ -156,7 +158,7 @@ public class OPlusExtras extends PreferenceFragment
             startActivity(intent);
             return true;
         });
-
+    
         // Kcal
         displayCategory = displayCategory | isFeatureSupported(context, R.bool.config_deviceSupportsKcal);
         if (isFeatureSupported(context, R.bool.config_deviceSupportsKcal)) {
@@ -235,6 +237,17 @@ public class OPlusExtras extends PreferenceFragment
         ps.oneShot(this.getView(),50);
         return true;
         });
+
+        /**
+         * Dolby
+         * The redundant condition could've been merged as this category only has a single config,
+         * but we keep it to maintain consistency with the rest of the code
+         **/
+        boolean dolbyCategory = false;
+        dolbyCategory = displayCategory | isFeatureSupported(context, R.bool.config_deviceSupportsDolby);
+        if (!dolbyCategory) {
+            getPreferenceScreen().removePreference((Preference) findPreference(KEY_CATEGORY_DOLBY));
+        }
 
         boolean cpuCategory = false;
 
@@ -503,7 +516,7 @@ public class OPlusExtras extends PreferenceFragment
             prefChange.putBoolean(KEY_AUTO_HBM_SWITCH, enabled).commit();
             FileUtils.enableService(getContext());
             return true;
-          } else if (preference == mFpsInfo) {
+        } else if (preference == mFpsInfo) {
             boolean enabled = (Boolean) newValue;
             Intent fpsinfo = new Intent(this.getContext(),
                     org.evolution.oplus.OPlusExtras.services.FPSInfoService.class);
